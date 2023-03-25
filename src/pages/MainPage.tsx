4308b4b317bc550a2b64edd1c5252f840c56502d
alyssa1996 <alyssapark96@gmail.com>
2023-03-25 11:21:28 +0900

    First setting

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Layout} from '../styles';
+
+const MainPage = () => {
+  return <Layout>hello world!</Layout>;
+};
+
+export default MainPage;
---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  margin: 0 auto;
+  width: 100vw;
+  max-width: 600px;
+  height: 100vh;
+  background-color: ivory;
+`;
