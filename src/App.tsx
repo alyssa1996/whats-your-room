37986393d54d2ff42d5d4229fd4ec37a46d16907
alyssa1996 <alyssapark96@gmail.com>
2023-04-05 21:43:31 +0900

    Link the url with location page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Route, Routes} from 'react-router-dom';
+import LocationPage from './pages/LocationPage';
 import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/location" element={<LocationPage />} />
     </Routes>
   );
 }
