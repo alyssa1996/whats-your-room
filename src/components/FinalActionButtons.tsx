62c7c3eadf21e2351f1983e7d9d99479442f97a0
alyssa1996 <alyssapark96@gmail.com>
2023-04-29 22:38:26 +0900

    Add disable feature on final button

--- src/components/FinalActionButtons.tsx
+++ src/components/FinalActionButtons.tsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Layout} from '../styles';
 
 interface ActionButtonProps {
   onHandleReset: () => void;
   onHandleConfirm: () => void;
+  isInitializeDisabled: boolean;
+  isConfirmDisabled: boolean;
 }
 
-const FinalActionButtons = ({onHandleReset, onHandleConfirm}: ActionButtonProps) => {
+const FinalActionButtons = ({onHandleReset, onHandleConfirm, isInitializeDisabled, isConfirmDisabled}: ActionButtonProps) => {
   return (
     <ButtonLayout>
-      <InitializeButton onClick={() => onHandleReset()}>초기화</InitializeButton>
-      <SelectButton onClick={() => onHandleConfirm()}>선택완료</SelectButton>
+      <InitializeButton
+        isInitializeDisabled={isInitializeDisabled}
+        onClick={() => {
+          // if (isInitializeDisabled) return;
+          onHandleReset();
+        }}
+      >
+        초기화
+      </InitializeButton>
+      <SelectConfirmButton
+        isConfirmDisabled={isConfirmDisabled}
+        onClick={() => {
+          // if (isConfirmDisabled) return;
+          onHandleConfirm();
+        }}
+      >
+        선택완료
+      </SelectConfirmButton>
     </ButtonLayout>
   );
 };
 
 export default FinalActionButtons;
 
-const ButtonLayout = styled(Layout)`
+const ButtonLayout = styled.div`
   width: 100%;
   height: auto;
   display: flex;
   position: fixed;
+  left: 0px;
   bottom: 0px;
 `;
 
-const InitializeButton = styled.div`
+const InitializeButton = styled.div<{isInitializeDisabled: boolean}>`
   width: 35%;
   padding: 17px 0px;
   margin: 0 auto;
@@ -34,9 +52,36 @@ const InitializeButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  color: ${({isInitializeDisabled}) => (isInitializeDisabled ? 'white' : '#2A2A2A')};
+  font-family: 'Gmarket Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+  /* identical to box height */
+
+  text-align: center;
+  letter-spacing: -0.042em;
 `;
 
-const SelectButton = styled(InitializeButton)`
+const SelectConfirmButton = styled.div<{isConfirmDisabled: boolean}>`
+  padding: 17px 0px;
   width: 65%;
-  background-color: #127f00;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({isConfirmDisabled}) => (isConfirmDisabled ? '#e4e4e4' : '#127f00')};
+
+  font-family: 'Gmarket Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 20px;
+  /* identical to box height */
+
+  text-align: center;
+  letter-spacing: -0.042em;
+
+  color: #ffffff;
 `;
